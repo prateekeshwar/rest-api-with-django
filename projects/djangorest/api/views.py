830f5fccade11a7dd287fd06a34ab3b94baa98a4
doney <doneybanaguna@gmail.com>
2017-06-11 01:37:08 +0530

    add rest api

---
+++ projects/djangorest/api/views.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from rest_framework import generics,permissions
+from .serializers import BucketlistSerializer
+from .models import Bucketlist
+from .permissions import IsOwner
+
+# Create your views here.
+class CreateView(generics.ListCreateAPIView):
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+        
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
+    permission_classes = (permissions.IsAuthenticated,IsOwner)
